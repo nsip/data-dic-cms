7d94c60bf5b53871bcaf00946968703ddbeded86
Qing Miao <digisanqm@outlook.com>
2022-09-25 21:26:08 +1000

    * add sign-in & sign-up page, TODO: link backend api

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,45 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-  <MainTitle />
 
-  <div id="container">
+    <SignPage v-if="!loginOK" />
 
-    <div id="left">
-      <EntryName />
-      <br>
-      <EntryOtherNames />
-      <br>
-      <EntryDef />
-      <br>
-      <EntrySIF />
-      <br>
-      <EntryOtherStd />
-      <br>
-      <EntryLegalDef />
-      <br>
-      <EntryCol />
-      <br>
-      <EntryMeta />
+    <div v-if="loginOK">
+        <MainTitle />
+        <div id="container">
+            <div id="left">
+                <EntryName />
+                <br>
+                <EntryOtherNames />
+                <br>
+                <EntryDef />
+                <br>
+                <EntrySIF />
+                <br>
+                <EntryOtherStd />
+                <br>
+                <EntryLegalDef />
+                <br>
+                <EntryCol />
+                <br>
+                <EntryMeta />
+            </div>
+            <div id="middle">
+                <PreviewContent />
+            </div>
+            <div id="right">
+                <GenJSON />
+            </div>
+        </div>
+        <EntryExport />
     </div>
 
-    <div id="middle">
-      <PreviewContent/>
-    </div>
-
-    <div id="right">
-      <GenJSON/>
-    </div>
-
-  </div>
-
-  <EntryExport />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { loginOK } from './components/share/share'
+import SignPage from './components/SignPage.vue';
 import MainTitle from './components/Title.vue';
 import EntryName from './components/Entry_1_Name.vue';
 import EntryOtherNames from './components/Entry_2_OtherName.vue';
@@ -52,159 +54,168 @@ import GenJSON from './components/GenJSON.vue'
 import EntryExport from './components/BtnExport.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    MainTitle,
-    EntryName,
-    EntryOtherNames,
-    EntryDef,
-    EntrySIF,
-    EntryOtherStd,
-    EntryLegalDef,
-    EntryCol,    
-    EntryMeta,
-    PreviewContent,
-    GenJSON,
-    EntryExport
-}
+    name: 'App',
+    components: {
+        SignPage,
+        MainTitle,
+        EntryName,
+        EntryOtherNames,
+        EntryDef,
+        EntrySIF,
+        EntryOtherStd,
+        EntryLegalDef,
+        EntryCol,
+        EntryMeta,
+        PreviewContent,
+        GenJSON,
+        EntryExport
+    },
+    setup() {
+        return {
+            loginOK
+        }
+    }
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-  margin-top: 10px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 10px;
 }
 
 /* layout */
 
 #container {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  /* margin-top: -10px; */
+    height: 100%;
+    width: 100%;
+    display: flex;
+    /* margin-top: -10px; */
 }
 
 #left {
-  width: 33%;
-  margin-right: 1%;
-  overflow: scroll;
-  /*   display: flex;
+    width: 33%;
+    margin-right: 1%;
+    overflow: scroll;
+    /*   display: flex;
     flex-direction: column; */
 }
 
 #middle {
-  width: 33%;  
-  background-color: rgb(240, 240, 240);
-  overflow: scroll;
-  /* display: flex;
+    width: 33%;
+    background-color: rgb(240, 240, 240);
+    overflow: scroll;
+    /* display: flex;
   flex-direction: column; */
 }
 
 #right {
-  width: 33%;
-  margin-left: 1%;
-  background-color: rgb(220, 220, 220);
-  overflow: scroll;
-  /*overflow-y: scroll;
+    width: 33%;
+    margin-left: 1%;
+    background-color: rgb(220, 220, 220);
+    overflow: scroll;
+    /*overflow-y: scroll;
   background-color: rgb(220, 220, 220); */
 }
 
 /*  */
 
 .line1 {
-  width: 100%;
-  text-align: left;
-  margin-left: 0;
-  margin-top: 10px;
-  color: #2c3e50;
+    width: 100%;
+    text-align: left;
+    margin-left: 0;
+    margin-top: 10px;
+    color: #2c3e50;
 }
 
 .line2 {
-  width: 90%;
-  text-align: left;
-  margin-right: 0;
-  margin-top: 20px;
-  /* border-top: 1px dotted black;
+    width: 90%;
+    text-align: left;
+    margin-right: 0;
+    margin-top: 20px;
+    /* border-top: 1px dotted black;
   border-bottom: 1px dotted black; */
 }
 
 /* components shared */
 
 .com {
-  border: 1px solid;
-  padding: 10px;
-  box-shadow: 5px 5px 5px 5px lightgray;
-  /* x 偏移量 | y 偏移量 | 阴影模糊半径 | 阴影扩散半径 | 阴影颜色 */
+    border: 1px solid;
+    padding: 10px;
+    box-shadow: 5px 5px 5px 5px lightgray;
+    /* x 偏移量 | y 偏移量 | 阴影模糊半径 | 阴影扩散半径 | 阴影颜色 */
 }
 
 .label {
-  font-weight: bold;
-  font-size: 16px;
-  font-style: italic;
-  color: rgb(90, 90, 90);
-  margin-left: 1%;
-  margin-bottom: 10px;
-  display: inline-block; /* set span */
+    font-weight: bold;
+    font-size: 16px;
+    font-style: italic;
+    color: rgb(90, 90, 90);
+    margin-left: 1%;
+    margin-bottom: 10px;
+    display: inline-block;
+    /* set span */
 }
 
 .hint1 {
-  font-weight: normal;
-  font-size: 13px;
-  font-style: italic;
-  color: rgb(120, 120, 120);
-  position: absolute;
-  left: 140px;
-  margin-top: 3px;
-  display: inline-block; /* set span */
-  cursor: pointer;
+    font-weight: normal;
+    font-size: 13px;
+    font-style: italic;
+    color: rgb(120, 120, 120);
+    position: absolute;
+    left: 140px;
+    margin-top: 3px;
+    display: inline-block;
+    /* set span */
+    cursor: pointer;
 }
 
 .hint2 {
-  font-weight: normal;
-  font-size: 13px;
-  font-style: italic;
-  color: rgb(120, 120, 120);
-  margin-left: 2%;
-  margin-bottom: 10px;
-  display: inline-block; /* set span */
-  cursor: pointer;
+    font-weight: normal;
+    font-size: 13px;
+    font-style: italic;
+    color: rgb(120, 120, 120);
+    margin-left: 2%;
+    margin-bottom: 10px;
+    display: inline-block;
+    /* set span */
+    cursor: pointer;
 }
 
 .more-editor {
-  font-size: medium;
-  color: red;
-  float: right;
-  margin-right: 5px;
-  margin-bottom: 5px;
-  width: 22px;
-  border: none;
+    font-size: medium;
+    color: red;
+    float: right;
+    margin-right: 5px;
+    margin-bottom: 5px;
+    width: 22px;
+    border: none;
 }
 
 .less-editor {
-  font-size: medium;
-  color: green;
-  float: right;
-  margin-right: 5px;
-  margin-bottom: 5px;
-  width: 22px;
-  border: none;
+    font-size: medium;
+    color: green;
+    float: right;
+    margin-right: 5px;
+    margin-bottom: 5px;
+    width: 22px;
+    border: none;
 }
 
 .hide-editor {
-  font-size: medium;
-  float: right;
-  color: blue;
-  margin-bottom: 5px;
-  width: 22px;
-  border: none;
+    font-size: medium;
+    float: right;
+    color: blue;
+    margin-bottom: 5px;
+    width: 22px;
+    border: none;
 }
 
 .subline {
-  color: rgb(227, 240, 245);
+    color: rgb(227, 240, 245);
 }
 </style>
---
+++ src/components/SignPage.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="login-page">
+
+        <div v-if="!registerActive">
+            <h1>Sign In</h1>
+            <form>
+                <input class="textbox" v-model="emailLogin" type="email" placeholder="Email" required>
+                <input class="textbox" v-model="passwordLogin" type="password" placeholder="Password" required>
+                <input class="btn1" type="submit" @click="doLogin" value="submit">
+                <p>Don't have an account? <a href="#" @click="doShift()">Sign up here</a> </p>
+            </form>
+        </div>
+
+        <div v-if="registerActive">
+            <h1>Sign Up</h1>
+            <form>
+                <input class="textbox" v-model="emailReg" type="email" placeholder="Email" required>
+                <input class="textbox" v-model="passwordReg" type="password" placeholder="Password" required>
+                <input class="textbox" v-model="confirmReg" type="password" placeholder="Confirm Password" required>
+                <input class="btn2" type="submit" @click="doRegister" value="submit">
+                <p>Already have an account? <a href="#" @click="doShift()">Sign in here</a> </p>
+            </form>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { loginOK } from './share/share'
+
+export default defineComponent({
+    name: 'SignPage',
+    setup() {
+
+        let registerActive = ref(false)
+        let emailLogin = ref("")
+        let passwordLogin = ref("")
+        let emailReg = ref("")
+        let passwordReg = ref("")
+        let confirmReg = ref("")
+
+        const doLogin = () => {
+
+            alert('doLogin')
+        }
+
+        const doRegister = () => {
+            
+            if (passwordReg.value != confirmReg.value) {
+                alert('password confirmation error')
+                return
+            }
+
+            alert('register successful')
+            loginOK.value = true
+        }
+
+        const doShift = () => {
+            registerActive.value = !registerActive.value
+        }
+
+        return {
+            registerActive,
+
+            emailLogin,
+            passwordLogin,
+
+            emailReg,
+            passwordReg,
+            confirmReg,
+
+            loginOK,
+
+            doLogin,
+            doRegister,
+            doShift,
+        }
+    }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+    margin-bottom: 3%;
+}
+
+.login-page {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.textbox {
+    margin-bottom: 2%;
+    width: 100%;
+}
+
+.btn1 {
+    float: right;
+    margin-right: -2%;
+}
+
+.btn2 {
+    float: right;
+    margin-right: -1%;
+}
+</style>
