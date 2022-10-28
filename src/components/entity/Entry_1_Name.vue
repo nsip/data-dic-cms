4fe1d827ac11705f670bc5579d205288ba3bf25d
Qing Miao <digisanqm@outlook.com>
2022-10-28 17:17:16 +1100

    * entity name from richtext to plain input

--- src/components/entity/Entry_1_Name.vue
+++ src/components/entity/Entry_1_Name.vue
@@ -1,74 +1,31 @@
 <template>
     <div class="com">
         <span class="label">{{ label }}</span>
-        <button class="hide-editor" @click="onToggleVisible()">
-            <font-awesome-icon :icon="icon" />
-        </button>
-        <span class="hint1">{{ hint }}</span>
-        <div :hidden="!visEditor">
-            <!-- essential, minimal, full, and ""  -->
-            <QuillEditor theme="snow" toolbar="essential" :placeholder="holder" @ready="onReady" @textChange="textChange" />
-        </div>
+        <input class="content" type="text" v-model="entity" placeholder="entity name">
     </div>
 </template>
 
 <script lang="ts">
-// npm install @vueup/vue-quill@beta --save
 
-import { defineComponent, ref, onMounted } from "vue";
-import { QuillEditor, Quill } from "@vueup/vue-quill";
-import "@vueup/vue-quill/dist/vue-quill.snow.css";
-import "@vueup/vue-quill/dist/vue-quill.bubble.css";
+import { defineComponent, ref, onMounted, watchEffect } from "vue";
 import { jsonEntityHTML, jsonEntityTEXT } from "../../share/EntityType";
 
 export default defineComponent({
     name: "EntryName",
-    components: {
-        QuillEditor,
-    },
     setup() {
         const label = "Entity:";
-        const hint = "entity name";
-        const holder = "entity name value";
-        let icon = ref("chevron-up");
-        let thisQuill: Quill;
-        let visEditor = ref(true);
-        let flagSet: boolean = true
-
-        const onReady = (quill: Quill) => {
-            thisQuill = quill;
-        };
-
-        const textChange = () => {
-            if (flagSet) {
-                jsonEntityHTML.SetName(thisQuill.root.innerHTML);
-                jsonEntityTEXT.SetName(thisQuill.getText(0, 100000));
-            }
-        };
-
-        const onToggleVisible = () => {
-            visEditor.value = !visEditor.value;
-            icon.value = icon.value == "chevron-down" ? "chevron-up" : "chevron-down";
-        };
-
+        const entity = ref("")
         onMounted(async () => {
             await new Promise((f) => setTimeout(f, 500));
-            flagSet = false
-            thisQuill.root.innerHTML = jsonEntityHTML.Entity;
-
-            await new Promise((f) => setTimeout(f, 500));
-            flagSet = true
+            entity.value = jsonEntityTEXT.Entity;
         })
-
+        watchEffect(() => {
+            jsonEntityHTML.SetName(entity.value)
+            jsonEntityTEXT.SetName(entity.value)
+        })
         return {
             label,
-            hint,
-            holder,
-            icon,
-            visEditor,
-            textChange,
-            onReady,
-            onToggleVisible,
+            entity,
         };
     },
 });
@@ -76,7 +33,14 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h4 {
-    margin-left: 5px;
+.label {
+    margin-top: 10px;
+}
+
+.content {
+    margin-left: 15px;
+    width: 82%;
+    padding-left: 1%;
+    font-weight: bold;
 }
 </style>
